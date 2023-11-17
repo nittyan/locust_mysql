393e09b5418bed32396fc806dadb76c2ad69bf92
nittyan <hnittyan@gmail.com>
2023-11-17 21:17:03 +0900

    クラスの導入

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+import dataclasses
 import sys
 import time
 
@@ -14,8 +15,14 @@ connection = mysql.connector.connect(
 )
 
 
+@dataclasses.dataclass
+class Log:
+    time: str
+    sql: str
+    hash: str
+
+
 class EchoUser(User):
-    # wait_time = constant(0.8)
 
     def wait_time(self):
         return 0.8
@@ -23,29 +30,29 @@ class EchoUser(User):
     def on_start(self):
         with open('parsed_general_log2.log') as f:
             reader = csv.reader(f)
-            self._sqls = [(row[3], row[4]) for row in reader]
+            self._logs: list[Log] = [Log(time=row[0], sql=row[3], hash=row[4]) for row in reader]
 
     @task
     def execute_sql(self):
         cursor = connection.cursor(buffered=True)
-        sql: tuple[str, str] = self._sqls.pop(0)
+        log: Log = self._logs.pop(0)
         start = time.time()
-        cursor.execute(sql[0])
+        cursor.execute(log.sql)
         end = time.time()
 
         connection.commit()
         result = 0
         if cursor.rowcount:
-            if not sql[0].lower().find('insert') > 0:
+            if not log.sql.lower().find('insert') > 0:
                 result = cursor.fetchall()
 
         events.request.fire(
             request_type='sql',
-            name=sql[1],
+            name=log.hash,
             response_time=float(end - start) * 1000,
             response_length=sys.getsizeof(result),
             # exception=
         )
         cursor.close()
-        if not self._sqls:
+        if not self._logs:
             self.stop()
